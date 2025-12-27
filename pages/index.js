fc3f1009392e770009b43489046bc2bb9517cdd6
Kai-Granville <161145259+Kai-Granville@users.noreply.github.com>
2025-12-27 13:04:00 +0000

    inital commit

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: 40, fontFamily: "sans-serif" }}>
+      <h1>AWIN Affiliate Site</h1>
+      <p>Deployment successful 🚀</p>
+    </main>
+  );
+}
